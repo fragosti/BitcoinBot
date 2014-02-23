13e551f13405b2badc3fe41f73c04fbb189b596c
Franesco Agosti <fra5@duke.edu>
2014-02-23 05:59:22 +0000

    about to work on graph

--- bitcoinbot.py
+++ bitcoinbot.py
@@ -55,11 +55,8 @@ def get_db():
 
 def new_bot(algorithm):
     api_key = get_api_key(session['user_id'])
-    print api_key
-    key = "AZGRIZYJ-H8VRF495-34H6CAF4-9UWI56WI-74U0063R" 
-    secret = "71eb80d6e1b60f4df6ae413cf36b44d1cdd30238fe82ef5a09416cfbb44e059e"
     handler = btceapi.KeyHandler(resaveOnDeletion=True)
-    handler.addKey(key, secret, 1)
+    handler.addKey(api_key['key'], api_key['secret'], 1)
     api = simwrapper.BTCESimulationApi(handler)
     algorithm = BasicAlgo(api)
     return Bot(algorithm, "ppc_usd")
