8320beba1c99bbd547cdaddd6fe26f30f6dc3b9e
ElliotOCo <elliot.oconnor@gmail.com>
2014-02-23 11:17:15 +0000

    near

--- static/js/transactions.js
+++ static/js/transactions.js
@@ -3,9 +3,23 @@ $('document').ready(function(){
 
 	
 	$.get('/api/transaction_history', function(response){
-		console.log(response)
+		var html = "";
+		response = JSON.parse(response)
+		
+		for(var i = 0; i < response.length ; i++){
+			obj = response[i];
+			console.log(obj)
+			html += 
+			"<tr>"+
+         		"<td>" + obj.time+ "</td>" +
+            	"<td>" + obj.pair + "</td>" + 
+            	"<td>" + obj.amount + "</td>" +
+            	"<td>" + obj.rate + "</td>" +
+        	"</tr>"
+		}
+		$('#transactions').html(html);
 	});
 
 
 
-})
+});
